a4c07c74add0bb71ea1575855463f5937f6b22ef
MetraJF <metravpn@gmail.com>
2026-05-08 14:31:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-const port = process.env.PORT || 3000; 
+// Définition du port dynamique (Render injecte la variable PORT)
+const port = process.env.PORT || 3000;
 
+// IMPORTANT : On ajoute '0.0.0.0' pour être visible sur le réseau Render
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Serveur d'acquisition prêt sur le port ${port}`);
+    console.log(`✅ Serveur validé et actif sur le port ${port}`);
 });
